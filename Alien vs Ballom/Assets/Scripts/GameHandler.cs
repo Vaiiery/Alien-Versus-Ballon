8c96b175a3f186d9b71992029b98f034adb73ebf
Vaiiery <55927707+Vaiiery@users.noreply.github.com>
2020-02-21 16:45:38 -0400

    Alliens VS Ballom proyect

---
+++ Alien vs Ballom/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+    public GameObject Nave;
+    public GameObject MenuPerder;
+    public static float health = 1f;
+
+    private void Start()
+    {
+       
+    
+        FunctionPeriodic.Create(() => {
+            if (health > 0) {
+                health -= .001f;
+                healthBar.SetSize(health);
+                MenuPerder.SetActive(false);
+
+                if (health  < .3f) {
+                    if ((health * 100f) % 3 == 0)
+                    {
+                        healthBar.SetColor(Color.green);
+                    }
+                    else {
+                        healthBar.SetColor(Color.red);
+                    }
+                    
+                }
+                if (health <= 0)
+                {
+                    Destruir();
+                }
+            }
+
+        }, .03f);
+
+        void Destruir()
+        {
+            Destroy(gameObject);
+            MenuPerder.SetActive(true);
+        }
+
+       
+    }
+
+}
